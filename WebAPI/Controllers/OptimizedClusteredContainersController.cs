a0e91cb9b9b7f4559a177166f443998f57bafc63
berkdemircioglu <berkdemircioglu@yahoo.com>
2022-09-09 13:13:36 +0300

    Add project files.

---
+++ WebAPI/Controllers/OptimizedClusteredContainersController.cs
@@ -0,0 +1,27 @@
+﻿using Business.Abstract;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OptimizedClusteredContainersController : ControllerBase
+    {
+        IContainerService _containerService; //To avoid instance generation process everytime,injection was implemented.
+
+        public OptimizedClusteredContainersController(IContainerService containerService)
+        {
+            _containerService = containerService;
+        }
+
+        [HttpGet]
+        public IActionResult GetOptimizedContainers(long vehicleId, int clusterNumber)
+        {
+            var result = _containerService.GetOptimizedContainers(vehicleId, clusterNumber);
+            if (result.Success)
+                return Ok(result);
+            return BadRequest(result);
+        }
+    }
+}
